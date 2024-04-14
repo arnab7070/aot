65da24f2d266e00edbbdcc015867867801b47edb
Bineet09 <bineetchattopadhyay00@gmail.com>
2024-04-14 14:12:31 +0530

    news bentogrid structure done

---
+++ app/news/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BentoGridDemo } from '../components/news';
+
+
+const page = () => {
+  return (
+    <div>
+      <BentoGridDemo/>
+    </div>
+  );
+}
+
+export default page;
