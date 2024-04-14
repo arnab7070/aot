55fe5da1577b5b11c0f8d091453cd8169589468f
Arnab Nandi <arnabnandi7074@gmail.com>
2024-04-14 12:27:25 +0530

    layout-grid for event section added

---
+++ app/events/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import EventSection from '../components/events'
+const Events = () => {
+  return (
+    <div>
+        <EventSection />
+    </div>
+  )
+}
+
+export default Events
