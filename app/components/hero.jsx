b1e84f28d1765bf21341a8454601d1284101f835
Supratik De <supratikde2@gmail.com>
2024-04-15 01:41:16 +0530

    video in hero

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Pacifico } from "next/font/google";
+import Typewriter from "typewriter-effect";
 const pacifico = Pacifico({
   weight: "400",
   subsets: ["latin"],
@@ -34,26 +35,58 @@ const Hero = () => {
   return (
     <>
       <section className="hero" style={parallax()}>
-        <h1 style={titleStyle()}>Parallax Hero</h1>
+        <video
+          aria-describedby="hero-video-description"
+          autoplay="autoplay"
+          loop="loop"
+          preload="auto"
+          poster="https://www.jhu.edu/assets/uploads/2024/02/homepage_loop_102_v2-Original-960x540.jpg"
+          muted="muted"
+          type="video/mp4"
+            src="https://www.jhu.edu/assets/uploads/2024/02/homepage_loop_102_v2-Original.mp4"
+        >
+        </video>
+
+        <h1 style={titleStyle()} className="text-center lg:text-left">
+          <Typewriter
+            options={{
+              strings: [
+                "Welcome to Academy Of Technology",
+                "Where aspirations meet success",
+                "Translating visions into reality",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h1>
       </section>
       <section className="content">
         <div className="jhu-block brand-message section brand" role="region">
           <div className="dot-matrix" aria-hidden="true"></div>
           <div className="center force">
             <div className="">
-              <h2 class="relative top-12 ">
-                <span class={`${pacifico.className} block text-4xl md:text-8xl ml-2 mr-auto md:ml-6 md:mr-auto`}>
+              <h2 className="relative top-12 ">
+                <span
+                  className={`${pacifico.className} block text-4xl md:text-8xl ml-2 mr-auto md:ml-6 md:mr-auto`}
+                >
                   When vision
                 </span>
                 <br />
-                <span class={`${pacifico.className} block text-4xl md:text-8xl ml-8 mr-auto md:ml-56`}>
+                <span
+                  className={`${pacifico.className} block text-4xl md:text-8xl ml-8 mr-auto md:ml-56`}
+                >
                   meets guidance,
                 </span>
                 <br />
-                <strong class="">
-                  <span class="block text-4xl md:text-8xl ml-40 mr-auto md:ml-[50rem] md:mr-auto"><h1 className="close">anything</h1></span>
+                <strong className="">
+                  <span className="block text-4xl md:text-8xl ml-40 mr-auto md:ml-[50rem] md:mr-auto">
+                    <h1 className="close">anything</h1>
+                  </span>
                   <br />
-                  <span class="block text-4xl md:text-8xl ml-20 mr-auto md:ml-96 md:mr-auto"><h1 className="close">is possible.</h1></span>
+                  <span className="block text-4xl md:text-8xl ml-20 mr-auto md:ml-96 md:mr-auto">
+                    <h1 className="close">is possible.</h1>
+                  </span>
                 </strong>
               </h2>
 
