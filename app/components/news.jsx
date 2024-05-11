cee9d536907d92eb90da1a9962cb1605e6088a57
Bineet09 <bineetchattopadhyay00@gmail.com>
2024-05-11 11:49:25 +0530

    news section done

--- app/components/news.jsx
+++ app/components/news.jsx
@@ -3,20 +3,23 @@ import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
 
 
-import {
-  IconArrowWaveRightUp,
-  IconBoxAlignRightFilled,
-  IconBoxAlignTopLeft,
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
-} from "@tabler/icons-react";
+// import {
+//   IconArrowWaveRightUp,
+//   IconBoxAlignRightFilled,
+//   IconBoxAlignTopLeft,
+//   IconClipboardCopy,
+//   IconFileBroken,
+//   IconSignature,
+//   IconTableColumn,
+// } from "@tabler/icons-react";
 import Image from "next/image";
 
 export function BentoGridDemo() {
   return (
+    <div className="max-w-4xl mx-auto font-bold">
+      NEWS SECTION
     <BentoGrid className="max-w-4xl mx-auto">
+      
       {items.map((item, i) => (
         <BentoGridItem
           key={i}
@@ -28,6 +31,7 @@ export function BentoGridDemo() {
         />
       ))}
     </BentoGrid>
+    </div>
   );
 }
 const Skeleton = ({ imageSrc }) => (
@@ -37,47 +41,52 @@ const Skeleton = ({ imageSrc }) => (
 );
 const items = [
   {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
+    title: "Professor of JU visited AOT",
+    description: "Dr. Ranjan Ganguly, Jadavpur University, visited on 4th May 2024, appreciated labs, addressed students.",
     // header: <Skeleton />,
-    imageSrc: "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    imageSrc: "https://media.licdn.com/dms/image/D4E22AQFIttyLobhh2w/feedshare-shrink_2048_1536/0/1715065222244?e=1718236800&v=beta&t=odhDBGU-vXBluWTIR9DjeMOvS7k-9ItPMqFcC5zYPj0",
+    // icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    title: "PCB Design Workshop",
+    description: "AOT held a PCB Design workshop by Dipak Chakraborty for EE students on 23rd march, offering practical insights.",
+    // header: <Skeleton />,
+    imageSrc: "https://media.licdn.com/dms/image/D4E22AQHWpXoPSWZVig/feedshare-shrink_2048_1536/0/1715062904545?e=1718236800&v=beta&t=5QGE5ufpMQ0E7Hr5eYvPoz0hol7wMawC49iVXhMGWd8",
+    // icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    title: "An industrial visit to BTPS",
+    description: "EE students visited BTPS on 24th April, gaining practical knowledge on theoretical concepts.",
+    // header: <Skeleton />,
+    imageSrc: "https://media.licdn.com/dms/image/D4E22AQENhSd0bpMpfQ/feedshare-shrink_2048_1536/0/1715063824018?e=1718236800&v=beta&t=zjVJPCZ_cyvRXkMUw2b0NHXB_n4-YuMMzwadJ7nvEOA",
+    // icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    title: "AOT and IEEE WIE Kolkata organized seminar on Women in Space Engineering.",
+    description: "AOT and IEEE WIE Kolkata jointly hosted a seminar on 'Women in Space Engineering,' showcasing initiatives supporting women in engineering. Dr. Sumitra Mukhopadhyay and Dr. Ankita Pramanik highlighted impactful efforts to empower women, drawing diverse attendees from students to faculty.",
+    // header: <Skeleton />,
+    imageSrc: "https://media.licdn.com/dms/image/D5622AQFIe7FuwQyw4w/feedshare-shrink_2048_1536/0/1713875963014?e=1718236800&v=beta&t=H3-jClfrMWs5GSGd2pE7FYnNZUx6rbpQ5nNziurIpm0",
+    // icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Pursuit of Knowledge",
-    description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton />,
-    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+    title: "Guardian Interaction: Academic Performance Discussion",
+    description: "AOT held a campus session on academic performance and attendance improvement, engaging with guardians and stakeholders interactively.",
+    // header: <Skeleton />,
+    imageSrc: "https://media.licdn.com/dms/image/D5622AQEdg8a9BzOMlg/feedshare-shrink_2048_1536/0/1713070848206?e=1718236800&v=beta&t=T6K5cAhaW5IrbGNmE5oBgs5l_xk2yEGRTwTOw2xwMgE",
+    // icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Joy of Creation",
-    description: "Experience the thrill of bringing ideas to life.",
-    header: <Skeleton />,
-    icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+    title: "Space Innovation: Dr. Tapan Misra",
+    description: "Dr. Tapan Misra discusses space tech innovation, past achievements, and future prospects.",
+    // header: <Skeleton />,
+    imageSrc: "https://media.licdn.com/dms/image/D5622AQF8bRwSnaDygw/feedshare-shrink_2048_1536/0/1712984307820?e=1718236800&v=beta&t=3mWle45qrhzFTRcDD0ju88YYlO-FxPNQzKTUlol8wgE",
+    // icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Spirit of Adventure",
-    description: "Embark on exciting journeys and thrilling discoveries.",
-    header: <Skeleton />,
-    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+    title: "Prize-Winning Start-Ups: Special Talk",
+    description: "ED-Cell hosts event with successful student entrepreneurs sharing startup journey, challenges, and advice",
+    // header: <Skeleton />,
+    imageSrc: "https://media.licdn.com/dms/image/D5622AQGFpYcl7apq9g/feedshare-shrink_2048_1536/0/1712983761618?e=1718236800&v=beta&t=Lyvi0NFY7OPFa9KKfnifmltu9Ogkbg3HfaCtdl4p-Rw",
+    // icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
 ];
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'i.ytimg.com'],
+        domains: ['images.unsplash.com', 'i.ytimg.com', 'media.licdn.com'],
         
     },
 };
