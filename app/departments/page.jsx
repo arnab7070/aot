7326d07400440a4b179534018fc1c6aa4f0e10e3
Arnab Nandi <arnabnandi7074@gmail.com>
2024-05-17 20:34:09 +0530

    view more

--- app/departments/page.jsx
+++ app/departments/page.jsx
@@ -18,42 +18,42 @@ const ugDepartments = [
     title: "Computer Science and Engineering",
     text: "Explore the world of computing with CSE.",
     imageUrl: "https://plus.unsplash.com/premium_photo-1678565869434-c81195861939",
-    btnText: "Read More",
+    btnText: "View More",
     link: 'cse'
   },
   {
     title: "Computer Science and Business Systems",
     text: "Integrate technology with business using CSBS.",
     imageUrl: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f",
-    btnText: "Read More",
+    btnText: "View More",
     link: 'csbs'
   },
   {
     title: "Electrical and Electronics Engineering",
     text: "Power up your knowledge in EEE.",
     imageUrl: "https://images.unsplash.com/photo-1596496181848-3091d4878b24",
-    btnText: "Read More",
+    btnText: "View More",
     link: 'eee'
   },
   {
     title: "Electronics and Communication Engineering",
     text: "Communicate the future with ECE.",
     imageUrl: "https://images.unsplash.com/photo-1603732551658-5fabbafa84eb",
-    btnText: "Read More",
+    btnText: "View More",
     link: 'ece'
   },
   {
     title: "Electrical Engineering",
     text: "Master the flow of electricity with EE.",
     imageUrl: "https://images.unsplash.com/photo-1517420704952-d9f39e95b43e",
-    btnText: "Read More",
+    btnText: "View More",
     link: 'ee'
   },
   {
     title: "Mechanical Engineering",
     text: "Engineer the world with ME.",
     imageUrl: "https://images.unsplash.com/photo-1599585896662-85853131f259",
-    btnText: "Read More",
+    btnText: "View More",
     link: 'me'
   },
 ];
@@ -63,7 +63,7 @@ const pgDepartments = [
     title: "Master of Computer Applications",
     text: "Advance your career with MCA.",
     imageUrl: "https://picsum.photos/500/300/?image=20",
-    btnText: "Read More",
+    btnText: "View More",
     link: 'mca'
   }
 ];
@@ -77,9 +77,9 @@ const Page = () => {
       <Navbar nav={nav} />
       <div className="main">
         <h1 className={`${courgette.className} z-[-9] text-6xl md:text-8xl mt-52 md:mt-5 relative font-bold text-center mb-6`}>Departments</h1>
-        
+
         <div className="department-section">
-        <h2 className={`${courgette.className} text-4xl text-center mb-4`}>UG Programs</h2>
+          <h2 className={`${courgette.className} text-4xl text-center mb-4`}>UG Programs</h2>
           <ul className="cards">
             {ugDepartments.map((dept, index) => (
               <li key={index} className="cards_item">
@@ -101,7 +101,7 @@ const Page = () => {
         </div>
 
         <div className="department-section">
-        <h2 className={`${courgette.className} text-4xl text-center mb-4`}>PG Programs</h2>
+          <h2 className={`${courgette.className} text-4xl text-center mb-4`}>PG Programs</h2>
           <ul className="cards">
             {pgDepartments.map((dept, index) => (
               <li key={index} className="cards_item">
@@ -122,7 +122,7 @@ const Page = () => {
           </ul>
         </div>
       </div>
-        
+
       <Footer />
     </>
   );
