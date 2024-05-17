15dc109e9ec5d0639d35391f55e51024aeae122a
Arnab Nandi <arnabnandi7074@gmail.com>
2024-05-17 12:53:53 +0530

    added hero section to dept page

--- app/cse/page.jsx
+++ app/cse/page.jsx
@@ -2,12 +2,13 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 import Banner from '../components/banner1';
-
+import Hero from '../components/hero1'
 const CSE = () => {
     const [nav, setNav] = useState(true);
   return (
     <div>
         <Banner setNav={setNav} />
+        <Hero />
     </div>
   )
 }
